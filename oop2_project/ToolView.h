13f513968b0681765162818c246dd9511f4b1d50
נחום נגה <nahumnoga@gmail.com>
2019-05-26 22:35:19 +0300

    game menu

--- oop2_project/ToolView.h
+++ oop2_project/ToolView.h
@@ -9,18 +9,34 @@ using std::string;
 
 /*
  * ToolView class
- *//*
+ */
 class ToolView :
 	public GUI::ImageButton
 {
 
 public:
 	// constructor
-	ToolView(sf::RenderWindow& window);
+	explicit ToolView(sf::RenderWindow& window);
+	// set tool
+	void setTool(Tool& tool);
+	// set ammo
+	void setAmmo(int ammQuntity);
+	// set cuurent tool
+	void setCurrTool(Tool& currTool) { m_currTool = currTool; }
+	// get cuurent tool
+	Tool& getCurrTool() { return m_currTool; }
+	// get ammo
+	int getAmmo() { return m_ammo; }
 	// convert to string
-	virtual string toString() const; // overide;
+	virtual string toString() const override; 
 private:
-
-};*/
+	// current tool
+	Tool m_currTool;
+	// num Of ammo
+	int m_ammo;
+	// init
+	void init();
+	
+};
 
 
