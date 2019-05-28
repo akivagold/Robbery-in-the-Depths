78fab6e37bf7a70b83798df21d2ab82a23612978
Matanel Abayof <MatanelAbayof@outlook.com>
2019-05-28 16:16:36 +0300

    shark

--- oop2_project/Shark.cpp
+++ oop2_project/Shark.cpp
@@ -4,10 +4,11 @@ Shark::Shark(sf::RenderWindow& window, GameScreen& gameScreen, int numOfLife)
 	: NPC(window, gameScreen, numOfLife)
 {
 	init();
-	// TODO setDamge();
 }
 
 void Shark::init()
 {
-	setAnimation("life");
+	setAnimation("life"); // TODO change animation
+	// TODO setDrawPriority()
+	// TODO setDamge();
 }
