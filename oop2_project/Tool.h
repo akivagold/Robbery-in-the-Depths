35906d8669b538675ed706a4fa093350412ea581
Matanel Abayof <MatanelAbayof@outlook.com>
2019-06-04 17:20:47 +0300

    anims

--- oop2_project/Tool.h
+++ oop2_project/Tool.h
@@ -17,7 +17,7 @@ public:
 	virtual void useTool() = 0;
 	// check if can use tool
 	virtual bool canUsingTool() const = 0;
-	//get my owner
+	// get my owner
 	Character* getMyOwner() { return m_owner; }
 	// get tool name
 	const string& getToolName() const { return m_toolName; }
@@ -27,7 +27,7 @@ public:
 	virtual string toString() const;
 protected:
 	// constructor
-	explicit Tool(Character* object, string toolName = " ");
+	explicit Tool(Character* object, string toolName = "");
 private:
 	// owner of tool
 	Character* m_owner;
--- oop2_project/Weapon.h
+++ oop2_project/Weapon.h
@@ -8,9 +8,16 @@ class Weapon :
 	public Tool
 {
 public:
-	enum WeaponType { };
+	// number of weapons
+	static const int NUM_OF_WEAPONS = 1;
+	// weapon type
+	enum WeaponType { AK47 };
+	// random weapon
+	static WeaponType randWeapon();
+	// convert to string
+	virtual string toString() const override { return "Weapon: { " + Tool::toString() + " }"; }
 protected:
-	using Tool::Tool;
-	
+	// constructor
+	using Tool::Tool;	
 };
 
